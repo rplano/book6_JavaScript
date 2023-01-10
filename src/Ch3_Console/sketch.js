37b8b3f8e2c4cbf923caabe1c4f9516bfa61e3da
rplano <81776901+rplano@users.noreply.github.com>
2023-01-10 20:46:36 +0100

    Add files via upload

---
+++ src/Ch3_Console/sketch.js
@@ -0,0 +1,26 @@
+
+async function setup() {
+  createConsole();
+  // setFont('30px Arial');
+  clear();
+  print('hi ');
+  println('there');
+
+  let n = await readLine("Enter your name: ");
+  println('Hello ' + n + '!');
+
+  let nr1 = await readInt('Enter number 1: ');
+  let nr2 = await readInt('Enter number 2: ');
+  println('The sum is: ' + (nr1 + nr2));
+
+  let nr3 = await readDouble('Enter double number: ');
+  println('Double is: ' + nr3);
+
+  await readLine("Hit Enter to clear...");
+  clear();
+}
+
+// function draw() {
+//   background(220);
+//   print('hi');
+// }
